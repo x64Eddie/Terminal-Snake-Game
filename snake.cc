8d448b13ee1a895b785a3f4ad6b17e5fe3437c68
Eddie <eherrerag.eh@gmail.com>
2018-12-24 20:10:10 -0700

    Testing the drawing function

--- snake.cc
+++ snake.cc
@@ -1,11 +1,10 @@
 
-#include <cstdlib>
-#include <ncureses.h>
+#include <iostram>
 
 bool gameOver;
 const int width = 20, height = 20;
 int x, y, fruitX, fruitY, score;
-enum Direction = {
+enum Direction {
     STOP = 0,
     LEFT,
     RIGHT,
@@ -14,68 +13,60 @@ enum Direction = {
 }
 Direction dir;
 
-void setup(){
-
-    initscr();
-    clear();
-    noecho();
-    cbreak();
-    curs_set(0);
-
+void Setup(){
 
     gameOver = false;
     dir = STOP;
     x = width / 2;
     y = height / 2;
-    fruitX = (rand()%width)+ 1;
-    fruitY = (rand()%height)+ 1;
+    fruitX = (rand()%width);
+    fruitY = (rand()%height);
     score = 0;
 
 }
 
-void draw(){
-    clear();
+void Draw(){
+    system("cls")//clear the teminal window
 
-    for(int i = 0; i < width + 2; i++){
-        mvprintw(0, i, "+");
+    //top wall
+    for(int i = 0; i < width; i++){
+        count << "#";
     }
-    for(int i = 0; i < height + 2; i++){
-        for(int j = 0; j < width + 2; j++){
-            if(i == 0 | i == 21){
-                mvprintw(i, j, "+");
+    count << endl;
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j ++){
+            
+            if(j == 0 | j == (width - 1)){
+                //we are going to print the walls here
+                count << "#";
             }
-            else if(j == 0 | j == 21){
-                mvprintw(i, j, "+");
-            }
-            else if(i == y && j == x){
-                mvprintw(i, j, "0");
-            }
-            else if(i == fruitY && j == fruitX){
-                mvprintw(i, j, "@");
+            else{
+                count << " ";
             }
         }
     }
-    mvprintw(23, 0, "Score %d", score);
-    refresh();
 
-    getch();
-    endwin();
+    //bottom wall
+    for(int i = 0; i < width; i++){
+        count << "#";
+    }
 }
 
-void input(){
+void Input(){
 
 }
 
-void logic(){
+void Logic(){
 
 }
 
 int main(){
-    setup();
+    Setup();
     while(!gameOver){
-        draw();
-        input();
-        logic();
+        Draw();
+        Input();
+        Logic();
     }
     return 0;
 }
