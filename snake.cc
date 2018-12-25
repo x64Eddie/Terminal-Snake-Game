59e3b1652e74f493ff5145eb22c45a342bb68b57
Eddie <eherrerag.eh@gmail.com>
2018-12-24 23:16:10 -0700

    I have movement for the snake, and the top and bottom wall are showing up. Now I have to figure out why are they disapearing when I press a key

--- snake.cc
+++ snake.cc
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <ncurses.h>
 
 bool gameOver;
 const int width = 20, height = 20;
@@ -23,56 +24,115 @@ void Setup(){
     fruitY = (rand()%height);
     score = 0;
 
+    initscr();
+    cbreak();
+    refresh();
 }
 
 void Draw(){
-    system("clear");//clear the teminal window
-
+//    system("clear");//clear the teminal window
+    clear();
     //top wall
     for(int i = 0; i < width; i++){
-        std::cout << "#";
+        mvprintw(0,i,"#");
     }
-    std::cout << std::endl;
 
     for(int i = 0; i < height; i++){
         for(int j = 0; j < width; j ++){
             
             if(j == 0 | j == (width -1)){
 		//we are going to print the walls here
-		std::cout << "#";
+		mvprintw(j,i,"#");
+		}
+	    if(i == y && j ==  x){
+		mvprintw(i,j,"O");;
+		}
+	    else if(i == fruitX && j == fruitY){
+		mvprintw(i,j,"F");
 		}
 	    else{
-		std::cout << " ";
+		mvprintw(i,j," ");
 		}
         }
-	std::cout << std::endl;
     }
 
     //bottom wall
     for(int i = 0; i < width; i++){
-        std::cout << "#";
+        mvprintw((width-1),i,"#");
     }
 
     //we are going to skip a line then we are going to put the score below the window
-    std::cout << std::endl;
-    std::cout << "Score: " << score << std::endl;
+    mvprintw(height+1, 0,"Score:");
 }
 
-void Input(){
+int kbhit(void){
+        int ch = getch();
+        if(ch != ERR){
+                ungetch(ch);
+                return 1;
+        }
+        else{
+                return 0;
+        }
+}
 
+
+void Input(){
+	if(kbhit()){
+		switch(getch()){
+			case 'a':
+				dir = LEFT;
+				refresh();				
+				break;
+			case 'd':
+				dir = RIGHT;
+				refresh();
+				break;
+			case 's':
+				dir = DOWN;
+				refresh();
+				break;
+			case 'w':
+				dir = UP;
+				refresh();
+				break;
+			case 'x':
+				gameOver = false;
+				refresh();
+				break;
+		}
+	}
+	
+	
 }
 
 void Logic(){
+	switch(dir){
+		case STOP:
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case LEFT:
+			x--;
+			break;
+		case UP:
+			y++;
+			break;
+		case DOWN:
+			y--;
+			break;
+	}
 
-}
+}   
 
 int main(){
     Setup();
-	Draw();
-//    while(!gameOver){
-//        Draw();
-//        Input();
-//        Logic();
-// }
+    while(!gameOver){
+        Draw();
+	std::cout << "pressed";
+        Input();
+        Logic();
+	 }
     return 0;
 }
