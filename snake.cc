08ee13ab59a85c88b98180d2d91dc5146b7d09da
Eddie <eherrerag.eh@gmail.com>
2018-12-24 19:40:39 -0700

    Finished the setup method that is going to indicate how the game is going to to start. We are going to set the game Over to false, set the size of the screen and where the fruit is going to appear, finally, we are going to set the score to 0

--- snake.cc
+++ snake.cc
@@ -2,7 +2,34 @@
 #include <cstdlib>
 #include <ncureses.h>
 
+bool gameOver;
+const int width = 20, height = 20;
+int x, y, fruitX, fruitY, score;
+enum Direction = {
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
+Direction dir;
+
 void setup(){
+
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(0);
+
+
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = (rand()%width)+ 1;
+    fruitY = (rand()%height)+ 1;
+    score = 0;
 
 }
 
@@ -19,5 +46,11 @@ void logic(){
 }
 
 int main(){
+    setup();
+    while(!gameOver){
+        draw();
+        input();
+        logic();
+    }
     return 0;
 }
